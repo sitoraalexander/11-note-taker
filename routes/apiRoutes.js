8f457dd31cd20f7d08723e6b3ab151056eb7b884
Sitora Alexander <sitoraalexander@gmail.com>
2021-01-18 22:50:50 -0500

    created API and HTML Routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const store = require("../db/store");
+
+router.get("/notes", (req,res) => {
+    store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post("/notes", (req,res) => {
+    console.log(req);
+    store
+    .addNotes(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", (req,res) => {
+    store
+    .removeNotes(req.params.id)
+    .then(() => res.json({ok: true}))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
